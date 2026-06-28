d57688e4179cecbd5691c018d459742918696922
JJ <tayjuenjie.tjj@gmail.com>
2026-06-28 17:17:12 +0800

    Add initial testing scripts for RTDE control and URScript communication

--- src/testing/square.py
+++ src/testing/square.py
@@ -1,6 +1,7 @@
 import rtde_control
 import rtde_receive
 import math
+import numpy as np
 
 URSIM_IP = "127.0.0.1"
 
@@ -10,4 +11,13 @@ def degtorad(degrees: list) -> list:
 rtde_c = rtde_control.RTDEControlInterface(URSIM_IP)
 rtde_r = rtde_control.RTDEReceiveInterface(URSIM_IP)
 
-transform_1 = [[]]
+kx, ky, kz = k
+
+k = np.array([
+    [0, -kz, ky]
+    [kz, 0, -kx]
+    [-ky, kx, 0]
+])
+
+np.eye(3)    # 3×3 identity
+np.eye(4)    # 4×4 identity
